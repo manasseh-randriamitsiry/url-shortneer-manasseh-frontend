132d6e9446eeea074a50547c8669a35fc9b8ddb6
manasseh <manassehrandriamitsiry@gmail.com>
2025-07-05 11:59:32 +0300

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-100 text-center py-6 px-6 border-t border-gray-200">
       <p className="text-gray-600">
-        Développé avec ❤️ avec{' '}
+        Développé avec{' '}
         <span className="font-semibold text-blue-600">NestJS</span> +{' '}
         <span className="font-semibold text-blue-600">React</span> +{' '}
         <span className="font-semibold text-blue-600">TypeScript</span>
